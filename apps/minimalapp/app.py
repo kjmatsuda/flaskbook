10cc1fa87868a38e7c6ff5134d6a39474f617290
Koji Matsuda <kjmatsuda1985@gmail.com>
2022-11-13 07:50:16 +0900

    1.2節 test_request_context de url_for の値を確認

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,17 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return "Hello, Flaskbook!"
 
+
 @app.route("/hello/<name>",
-    methods=["GET","POST"],
-    endpoint="hello-endpoint")
+           methods=["GET", "POST"],
+           endpoint="hello-endpoint")
 def hello(name):
     return f"Hello, {name}!"
+
 
 @app.route("/name/<name>")
 def show_name(name):
     return render_template("index.html", name=name)
+
+
+with app.test_request_context():
+    # /
+    print(url_for("index"))
+
+    # /hello/world
+    print(url_for("hello-endpoint", name="world"))
+
+    # /name/ichiro?page=1
+    print(url_for("show_name", name="ichiro", page="1"))
