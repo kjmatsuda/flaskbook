e9890a8f20b6273565d60ce4d12a7e10b636c789
Koji Matsuda <kjmatsuda1985@gmail.com>
2022-11-20 11:30:30 +0900

    3.4節 ログイン機能のフォームクラスを作成する

--- apps/auth/forms.py
+++ apps/auth/forms.py
@@ -28,3 +28,22 @@ class SignUpForm(FlaskForm):
     )
 
     submit = SubmitField("新規登録")
+
+
+class LoginForm(FlaskForm):
+    email = StringField(
+        "メールアドレス",
+        validators=[
+            DataRequired(message="メールアドレスは必須です。"),
+            Email(message="メールアドレスの形式で入力してください。"),
+        ],
+    )
+
+    password = PasswordField(
+        "パスワード",
+        validators=[
+            DataRequired(message="パスワードは必須です。"),
+        ],
+    )
+
+    submit = SubmitField("ログイン")
