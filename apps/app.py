2d855756f903fb61ebd5dec0560f24070ba23829
Koji Matsuda <kjmatsuda1985@gmail.com>
2022-11-20 10:43:57 +0900

    3.3節 サインアップ機能のフォームクラスを作成する

--- apps/app.py
+++ apps/app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
 from apps.config import config
+
+login_manager = LoginManager()
+# 未ログイン時にリダイレクトするエンドポイントを指定
+login_manager.login_view = "auth.signup"
+# ログイン後に表示するメッセージを指定
+login_manager.login_message = ""
 
 db = SQLAlchemy()
 csrf = CSRFProtect()
@@ -13,6 +20,7 @@ def create_app(config_key):
     app = Flask(__name__)
     app.config.from_object(config[config_key])
 
+    login_manager.init_app(app)
     csrf.init_app(app)
 
     db.init_app(app)
