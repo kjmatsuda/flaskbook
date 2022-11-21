1316b1ba9d03da6c6b6d36629eea8d2688c8e642
Koji Matsuda <kjmatsuda1985@gmail.com>
2022-11-22 06:48:58 +0900

    4.3節 bootstrap4.0 を設置し、画像一覧画面のテンプレートを作成する

--- apps/app.py
+++ apps/app.py
@@ -31,6 +31,6 @@ def create_app(config_key):
     from apps.crud import views as crud_views
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
     from apps.detector import views as dt_views
-    app.register_blueprint(dt_views.detector)
+    app.register_blueprint(dt_views.dt)
 
     return app
--- apps/detector/views.py
+++ apps/detector/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 
 dt = Blueprint("detector", __name__, template_folder="templates")
 
+
 @dt.route("/")
 def index():
     return render_template("detector/index.html")
