285056c714020549265fec957db1413dc0526a2e
Koji Matsuda <kjmatsuda1985@gmail.com>
2022-11-26 10:54:42 +0900

    UploadImageForm のエラー修正

--- apps/detector/forms.py
+++ apps/detector/forms.py
@@ -7,8 +7,7 @@ class UploadImageForm(FlaskForm):
     image = FileField(
         validators=[
             FileRequired("画像ファイルを指定してください。"),
-            FileAllowed(["png", "jpg", "jpeg"]),
-            "サポートされていない画像形式です。",
+            FileAllowed(["png", "jpg", "jpeg"], "サポートされていない画像形式です。"),
         ]
     )
     submit = SubmitField("アップロード")
