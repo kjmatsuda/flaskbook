22b5e8427e4335ee469c86680fb0bb3dfad98c71
Koji Matsuda <kjmatsuda1985@gmail.com>
2022-11-27 16:04:06 +0900

    8.4節 画像処理系関数

--- apps/detector/views.py
+++ apps/detector/views.py
@@ -1,6 +1,11 @@
+import random
 import uuid
 from pathlib import Path
 
+import cv2
+import numpy as np
+import torch
+import torchvision
 from apps.app import db
 from apps.crud.models import User
 from apps.detector.forms import UploadImageForm
@@ -52,3 +57,40 @@ def upload_image():
         return redirect(url_for("detector.index"))
 
     return render_template("detector/upload.html", form=form)
+
+
+def make_color(labels):
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in labels]
+    color = random.choice(colors)
+    return color
+
+
+def make_line(result_image):
+    line = round(0.002 * max(result_image.shape[0:2])) + 1
+    return line
+
+
+def draw_lines(c1, c2, result_image, line, color):
+    cv2.rectangle(result_image, c1, c2, color, thickness=line)
+    return cv2
+
+
+def draw_texts(result_image, line, c1, cv2, color, labels, label):
+    display_txt = f"{labels[label]}"
+    font = max(line - 1, 1)
+    t_size = cv2.getTextSize(
+        display_txt, 0, fontScale=line / 3, thickness=font
+    )[0]
+    c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+    cv2.rectangle(result_image, c1, c2, color, -1)
+    cv2.putText(
+        result_image,
+        display_txt,
+        (c1[0], c1[1] - 2),
+        0,
+        line / 3,
+        [255, 255, 255],
+        thickness=font,
+        lineType=cv2.LINE_AA,
+    )
+    return cv2
