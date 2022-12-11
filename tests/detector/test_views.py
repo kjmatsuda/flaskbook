c02442f9d75258cf876b24b894a8876be35d1131
Koji Matsuda <kjmatsuda1985@gmail.com>
2022-12-11 14:46:56 +0900

    11.3 バリデートエラー時

--- tests/detector/test_views.py
+++ tests/detector/test_views.py
@@ -1,3 +1,9 @@
+from pathlib import Path
+
+from flask.helpers import get_root_path
+from werkzeug.datastructures import FileStorage
+
+
 def test_index(client):
     rv = client.get("/")
     assert "ログイン" in rv.data.decode()
@@ -33,3 +39,27 @@ def test_upload_signup_get(client):
     signup(client, "admin", "flaskbook@example.com", "password")
     rv = client.get("/upload", follow_redirects=True)
     assert "アップロード" in rv.data.decode()
+
+
+def upload_image(client, image_path):
+    """画像をアップロードする"""
+    image = Path(get_root_path("tests"), image_path)
+
+    test_file = (
+        FileStorage(
+            stream=open(image, "rb"),
+            filename=Path(image_path).name,
+            content_type="multipart/form-data",
+        )
+    )
+
+    data = dict(
+        image=test_file,
+    )
+    return client.post("/upload", data=data, follow_redirects=True)
+
+
+def test_upload_signup_post_validate(client):
+    signup(client, "admin", "flaskbook@example.com", "password")
+    rv = upload_image(client, "detector/testdata/test_invalid_file.txt")
+    assert "サポートされていない画像形式です。" in rv.data.decode()
