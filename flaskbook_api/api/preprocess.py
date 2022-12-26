9ef4d21711cd38ab5889db430056a3b8078cac3f
Koji Matsuda <kjmatsuda1985@gmail.com>
2022-12-26 21:45:31 +0900

    fix typo

--- flaskbook_api/api/preprocess.py
+++ flaskbook_api/api/preprocess.py
@@ -3,5 +3,5 @@ import torchvision
 
 def image_to_tensor(image):
     """画像データをテンソル型の数値データへ変換"""
-    image_tensor = torchvision.transforms.funtional.to_tensor(image)
+    image_tensor = torchvision.transforms.functional.to_tensor(image)
     return image_tensor
